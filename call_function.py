d1d5aee07347fa1cd461592c9be0e7103143c5dd
SomeGuyOnTheWeb650 <pollockjosef@gmail.com>
2026-03-14 18:04:45 -0700

    created a schema for getfilesinfo.py, created call_function.py to store schemas, and adjust system prompt to return call function responses, while allowing LLM to see my functions

---
+++ call_function.py
@@ -0,0 +1,5 @@
+from google.genai import types
+from functions import get_files_info, get_file_content, write_file, run_python_file
+available_functions = types.Tool(
+    function_declarations=[get_files_info.schema_get_files_info],
+)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os, argparse
 from dotenv import load_dotenv
 from google.genai import types
 from prompts import system_prompt
-
+from call_function import *
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
 parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
@@ -20,7 +20,9 @@ client = genai.Client(api_key=api_key)
 response = client.models.generate_content(
     model= "gemini-2.5-flash", 
     contents= messages, 
-    config=types.GenerateContentConfig(system_instruction=system_prompt),
+    config=types.GenerateContentConfig(
+        tools=[available_functions], system_instruction=system_prompt
+    ),
 )
 if response.usage_metadata == None:
     raise RuntimeError("prompt didn't run correctly")
@@ -28,8 +30,15 @@ if args.verbose:
 
     print(f"User prompt: {args.user_prompt}\nPrompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
 
-print(response.text)
 
+func_objects = response.function_calls
+
+if len(func_objects) > 0:
+
+    for ob in func_objects:
+        print(f"Calling function: {ob.name}({ob.args})")
+else:
+    print(response.text)
 
 
     
