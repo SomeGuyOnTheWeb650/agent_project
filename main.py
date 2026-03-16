cf3a7a563b692ccd8ccf6493f3edd9cbd73ea4e5
SomeGuyOnTheWeb650 <pollockjosef@gmail.com>
2026-03-15 17:44:12 -0700

    final* version ai agent project

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, argparse
+import os, argparse, sys
 from dotenv import load_dotenv
 from google.genai import types
 from prompts import system_prompt
@@ -17,38 +17,49 @@ if api_key == None:
     raise RuntimeError("api_key wasn't found, check that .env is working correctly")
 from google import genai
 client = genai.Client(api_key=api_key)
-response = client.models.generate_content(
-    model= "gemini-2.5-flash", 
-    contents= messages, 
-    config=types.GenerateContentConfig(
-        tools=[available_functions], system_instruction=system_prompt
-    ),
-)
-if response.usage_metadata == None:
-    raise RuntimeError("prompt didn't run correctly")
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response token: {response.usage_metadata.candidates_token_count}")
+for i in range(20):
+    response = client.models.generate_content(
+        model= "gemini-2.5-flash", 
+        contents= messages, 
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt,
+        ),
+    )
+# possibly change scope below
+    if response.usage_metadata == None:
+        raise RuntimeError("prompt didn't run correctly")
+    
+    if len(response.candidates) > 0:
+        for mess in response.candidates:
+            messages.append(mess.content)
 
-func_objects = response.function_calls
-func_results = []
-if len(func_objects) > 0:
+    func_objects = response.function_calls
+    func_results = []
+    
+    if func_objects:
 
-    for ob in func_objects:
-        function_call_result = call_function(ob, args.verbose)
-        if len(function_call_result.parts) == 0:
-            raise Exception("Error: Empty parts list")
-        if function_call_result.parts[0].function_response == None:
-            raise Exception("Error: .part.functionresponse == None")
-        if function_call_result.parts[0].function_response.response == None:
-            raise Exception("Error: function_call_result.parts[0].function_response == None")
-        func_results.append(function_call_result.parts[0]) 
-        if args.verbose:
-            print(f"-> {function_call_result.parts[0].function_response.response}")
-        
+        for ob in func_objects:
+            function_call_result = call_function(ob, args.verbose)
+            if len(function_call_result.parts) == 0:
+                raise Exception("Error: Empty parts list")
+            if function_call_result.parts[0].function_response == None:
+                raise Exception("Error: .part.functionresponse == None")
+            if function_call_result.parts[0].function_response.response == None:
+                raise Exception("Error: function_call_result.parts[0].function_response == None")
+            func_results.append(function_call_result.parts[0]) 
+            if args.verbose:
+                
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+        messages.append(types.Content(role="user", parts=func_results))
+        if i == 19:
+            print("Possible Error: Reached maximum established prompts: 20")
+            sys.exit(1)
+    
+    
 
-
-else:
-    print(response.text)
+    else:
+        print(response.text)
+        break
 
 
     
--- prompts.py
+++ prompts.py
@@ -4,6 +4,9 @@ You are a helpful AI coding agent.
 When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
 
 - List files and directories
+- Get a file's contents.
+- Write some text to a file (don't overwrite anything important, maybe create a new file).
+- Execute the calculator app's tests (test.py)
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
